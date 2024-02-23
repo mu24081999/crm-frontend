67d899d32d434f95ecb9bd88a885c9be576ea4cc
mu24081999 <116753849+mu24081999@users.noreply.github.com>
2024-02-23 15:54:53 +0500

    m

--- src/App.js
+++ src/App.js
@@ -4,6 +4,35 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+// import "./assets/distt/css/style.css";
+// import "./assets/vendors/daterangepicker/daterangepicker.css";
+// import "./assets/vendors/datatables.net-bs5/css/dataTables.bootstrap5.min.css";
+// import "./assets/vendors/datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css";
+// import "./assets/vendors/dropify/dist/css/dropify.min.css";
+// import "";
+// import "";
+// import "";
+// import "";
+// import "";
+// import "";
+// import "";
+// import "";
+// import "";
+// import "";
+// import "";
+// import "";
+// import "";
+// import "";
+// import "";
+// import "";
+// import "";
+// import "";
+// import "";
+// import "";
+// import "";
+// import "";
+
 const App = () => {
   return (
     <>
