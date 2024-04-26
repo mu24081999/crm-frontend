903d504beaca061a4aaff654223e4295a80730c2
mu24081999 <116753849+mu24081999@users.noreply.github.com>
2024-04-27 00:54:53 +0500

    updated

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-let sslOptions;
 
 // Serve static files from the 'build' directory for all hostnames
 // app.use(express.static(path.join(__dirname, "build")));
@@ -13,22 +12,8 @@ let sslOptions;
 app.use((req, res, next) => {
   if (req.hostname === "app.desktopcrm.com") {
     express.static(path.join(__dirname, "build"))(req, res, next);
-
-    sslOptions = {
-      key: fs.readFileSync("app.desktopcrm.com.key"),
-      cert: fs.readFileSync("app_desktopcrm_com.crt"),
-      // cert: fs.readFileSync("desktopcrm_com.crt"),
-      ca: fs.readFileSync("app_desktopcrm_com.ca-bundle"),
-      // ca: fs.readFileSync("desktopcrm.ca-bundle"),
-    };
   } else if (req.hostname === "desktopcrm.com") {
     express.static(path.join(__dirname, "build2"))(req, res, next);
-    // Load SSL certificate and key
-    sslOptions = {
-      key: fs.readFileSync("desktopcrm.key"),
-      cert: fs.readFileSync("desktopcrm.crt"),
-      ca: fs.readFileSync("desktopcrm.ca-bundle"),
-    };
   } else {
     next();
   }
@@ -38,6 +23,20 @@ app.use((req, res, next) => {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+// Load SSL certificate and key
+// const sslOptions = {
+//   key: fs.readFileSync("desktopcrm.key"),
+//   cert: fs.readFileSync("desktopcrm.crt"),
+//   ca: fs.readFileSync("desktopcrm.ca-bundle"),
+// };
+const sslOptions = {
+  key: fs.readFileSync("app.desktopcrm.com.key"),
+  cert: fs.readFileSync("app_desktopcrm_com.crt"),
+  // cert: fs.readFileSync("desktopcrm_com.crt"),
+  ca: fs.readFileSync("app_desktopcrm_com.ca-bundle"),
+  // ca: fs.readFileSync("desktopcrm.ca-bundle"),
+};
+console.log("🚀 ~ sslOptions:", path.join(__dirname, "build"));
 
 // Start HTTPS server
 https.createServer(sslOptions, app).listen(443, () => {
