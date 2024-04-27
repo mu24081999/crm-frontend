e330adcfa3b937617c47550db5ebef1e417e0d77
mu24081999 <116753849+mu24081999@users.noreply.github.com>
2024-04-27 20:59:45 +0500

    updated

--- server.js
+++ server.js
@@ -56,5 +56,5 @@ app.use((req, res, next) => {
 // Error handling
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send("Something broke!");
+  res.status(500).send("Something broken!");
 });
