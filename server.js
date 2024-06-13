93bbb2fc67aa7424910189ee2b3e0cd37f81ade7
mu24081999 <116753849+mu24081999@users.noreply.github.com>
2024-06-13 23:12:02 +0500

    updated

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const path = require("path");
 const passport = require("passport");
 const app = express();
+var GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 // Serve static files from the 'build' directory for all hostnames
 // app.use(express.static(path.join(__dirname, "build")));
@@ -19,7 +20,32 @@ app.use((req, res, next) => {
     next();
   }
 });
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        "73457248543-vn1bjkn98qogdcljl35job6ek20e82qt.apps.googleusercontent.com",
+      clientSecret: "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",
+      callbackURL: "https://app.desktopcrm.com/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+      // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      //   return cb(err, user);
+      // });
+    }
+  )
+);
 
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
 // Handle all other requests
 app.get("*", (req, res, next) => {
   if (req.hostname === "app.desktopcrm.com") {
@@ -33,17 +59,23 @@ app.get("*", (req, res, next) => {
     res.sendFile(path.join(__dirname, "build2", "index.html"));
   }
 });
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
 app.get("/auth/google/callback", (req, res) => {
   console.log("🚀 ~ app.get ~ req:", req.url, req.params);
   console.log("yes");
   passport.authenticate(
     "google",
-    { failureRedirect: "/" },
-    (err, user, info) => {
-      console.log(user, err, info);
+    { failureRedirect: "/sign-in" },
+    (req, res) => {
+      console.log(req.user);
+      res.json({ success: true, user: req.user });
     }
   );
-  res.json({ success: true });
 });
 const sslOptionsSub = {
   key: fs.readFileSync("desktopcrm.key"),
