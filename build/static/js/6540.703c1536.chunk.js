d7c49fb067e5dd06941a19a17faed1f9b513039a
mu24081999 <116753849+mu24081999@users.noreply.github.com>
2024-07-26 20:44:05 +0500

    updated

---
+++ build/static/js/6540.703c1536.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[6540],{56540:(l,t,e)=>{e.d(t,{QYR:()=>c});var a=e(72119);function c(l){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M275.5 96l-96 96h-96v128h96l96 96V96zm50.863 89.637l-12.726 12.726L371.273 256l-57.636 57.637 12.726 12.726L384 268.727l57.637 57.636 12.726-12.726L396.727 256l57.636-57.637-12.726-12.726L384 243.273l-57.637-57.636z"},child:[]}]})(l)}}}]);
+//# sourceMappingURL=6540.703c1536.chunk.js.map
